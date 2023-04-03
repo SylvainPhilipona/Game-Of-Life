3631fccea791b168894aff46483e177f1244f409
SylvainPhilipona <sylvain.philipona.sp@gmail.com>
2023-04-03 08:35:13 +0200

    Release

---
+++ GameOfLife/GameOfLife/GameOfLife.Designer.cs
@@ -0,0 +1,216 @@
+﻿namespace GameOfLife
+{
+    partial class GameOfLife
+    {
+        /// <summary>
+        /// Variable nécessaire au concepteur.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Nettoyage des ressources utilisées.
+        /// </summary>
+        /// <param name="disposing">true si les ressources managées doivent être supprimées ; sinon, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Code généré par le Concepteur Windows Form
+
+        /// <summary>
+        /// Méthode requise pour la prise en charge du concepteur - ne modifiez pas
+        /// le contenu de cette méthode avec l'éditeur de code.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panContainer = new System.Windows.Forms.Panel();
+            this.btnStartAndStop = new System.Windows.Forms.Button();
+            this.tbSpeed = new System.Windows.Forms.TrackBar();
+            this.lblSpeed = new System.Windows.Forms.Label();
+            this.numSize = new System.Windows.Forms.NumericUpDown();
+            this.lblGridSize = new System.Windows.Forms.Label();
+            this.btnClear = new System.Windows.Forms.Button();
+            this.btnRandomGrid = new System.Windows.Forms.Button();
+            this.btnLastGeneration = new System.Windows.Forms.Button();
+            this.btnSingleGeneration = new System.Windows.Forms.Button();
+            this.lblStats = new System.Windows.Forms.Label();
+            this.pbGridGeneration = new System.Windows.Forms.ProgressBar();
+            ((System.ComponentModel.ISupportInitialize)(this.tbSpeed)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numSize)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // panContainer
+            // 
+            this.panContainer.Location = new System.Drawing.Point(16, 15);
+            this.panContainer.Margin = new System.Windows.Forms.Padding(4);
+            this.panContainer.Name = "panContainer";
+            this.panContainer.Size = new System.Drawing.Size(667, 615);
+            this.panContainer.TabIndex = 0;
+            // 
+            // btnStartAndStop
+            // 
+            this.btnStartAndStop.Location = new System.Drawing.Point(691, 15);
+            this.btnStartAndStop.Margin = new System.Windows.Forms.Padding(4);
+            this.btnStartAndStop.Name = "btnStartAndStop";
+            this.btnStartAndStop.Size = new System.Drawing.Size(333, 62);
+            this.btnStartAndStop.TabIndex = 1;
+            this.btnStartAndStop.Text = "Start";
+            this.btnStartAndStop.UseVisualStyleBackColor = true;
+            this.btnStartAndStop.Click += new System.EventHandler(this.btnStartAndStop_Click);
+            // 
+            // tbSpeed
+            // 
+            this.tbSpeed.LargeChange = 50;
+            this.tbSpeed.Location = new System.Drawing.Point(691, 362);
+            this.tbSpeed.Margin = new System.Windows.Forms.Padding(4);
+            this.tbSpeed.Maximum = 250;
+            this.tbSpeed.Minimum = 20;
+            this.tbSpeed.Name = "tbSpeed";
+            this.tbSpeed.Size = new System.Drawing.Size(333, 56);
+            this.tbSpeed.SmallChange = 20;
+            this.tbSpeed.TabIndex = 2;
+            this.tbSpeed.TickFrequency = 50;
+            this.tbSpeed.Value = 20;
+            this.tbSpeed.Scroll += new System.EventHandler(this.tbSpeed_Scroll);
+            // 
+            // lblSpeed
+            // 
+            this.lblSpeed.Location = new System.Drawing.Point(691, 400);
+            this.lblSpeed.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.lblSpeed.Name = "lblSpeed";
+            this.lblSpeed.Size = new System.Drawing.Size(333, 18);
+            this.lblSpeed.TabIndex = 3;
+            this.lblSpeed.Text = "Speed : xxms";
+            this.lblSpeed.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // numSize
+            // 
+            this.numSize.Location = new System.Drawing.Point(694, 437);
+            this.numSize.Margin = new System.Windows.Forms.Padding(4);
+            this.numSize.Name = "numSize";
+            this.numSize.Size = new System.Drawing.Size(330, 22);
+            this.numSize.TabIndex = 4;
+            this.numSize.ValueChanged += new System.EventHandler(this.NumSize_ValueChanged);
+            // 
+            // lblGridSize
+            // 
+            this.lblGridSize.Location = new System.Drawing.Point(688, 463);
+            this.lblGridSize.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.lblGridSize.Name = "lblGridSize";
+            this.lblGridSize.Size = new System.Drawing.Size(336, 21);
+            this.lblGridSize.TabIndex = 5;
+            this.lblGridSize.Text = "Taille de la grille : xx";
+            this.lblGridSize.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // btnClear
+            // 
+            this.btnClear.Location = new System.Drawing.Point(691, 290);
+            this.btnClear.Margin = new System.Windows.Forms.Padding(4);
+            this.btnClear.Name = "btnClear";
+            this.btnClear.Size = new System.Drawing.Size(333, 62);
+            this.btnClear.TabIndex = 6;
+            this.btnClear.Text = "Clear";
+            this.btnClear.UseVisualStyleBackColor = true;
+            this.btnClear.Click += new System.EventHandler(this.BtnClear_Click);
+            // 
+            // btnRandomGrid
+            // 
+            this.btnRandomGrid.Location = new System.Drawing.Point(691, 222);
+            this.btnRandomGrid.Margin = new System.Windows.Forms.Padding(4);
+            this.btnRandomGrid.Name = "btnRandomGrid";
+            this.btnRandomGrid.Size = new System.Drawing.Size(333, 62);
+            this.btnRandomGrid.TabIndex = 7;
+            this.btnRandomGrid.Text = "Random";
+            this.btnRandomGrid.UseVisualStyleBackColor = true;
+            this.btnRandomGrid.Click += new System.EventHandler(this.BtnRandomGrid_Click);
+            // 
+            // btnLastGeneration
+            // 
+            this.btnLastGeneration.Location = new System.Drawing.Point(691, 153);
+            this.btnLastGeneration.Margin = new System.Windows.Forms.Padding(4);
+            this.btnLastGeneration.Name = "btnLastGeneration";
+            this.btnLastGeneration.Size = new System.Drawing.Size(333, 62);
+            this.btnLastGeneration.TabIndex = 8;
+            this.btnLastGeneration.Text = "Back";
+            this.btnLastGeneration.UseVisualStyleBackColor = true;
+            this.btnLastGeneration.Click += new System.EventHandler(this.BtnLastGeneration_Click);
+            // 
+            // btnSingleGeneration
+            // 
+            this.btnSingleGeneration.Location = new System.Drawing.Point(691, 84);
+            this.btnSingleGeneration.Margin = new System.Windows.Forms.Padding(4);
+            this.btnSingleGeneration.Name = "btnSingleGeneration";
+            this.btnSingleGeneration.Size = new System.Drawing.Size(333, 62);
+            this.btnSingleGeneration.TabIndex = 9;
+            this.btnSingleGeneration.Text = "1 generation";
+            this.btnSingleGeneration.UseVisualStyleBackColor = true;
+            this.btnSingleGeneration.Click += new System.EventHandler(this.BtnSingleGeneration_Click);
+            // 
+            // lblStats
+            // 
+            this.lblStats.Location = new System.Drawing.Point(694, 493);
+            this.lblStats.Name = "lblStats";
+            this.lblStats.Size = new System.Drawing.Size(325, 55);
+            this.lblStats.TabIndex = 10;
+            this.lblStats.Text = "Nombre de générations : xx\r\nNombre de cellules vivantes : xx\r\nNombre de cellules " +
+    "mortes : xx\r\n";
+            this.lblStats.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // pbGridGeneration
+            // 
+            this.pbGridGeneration.Location = new System.Drawing.Point(697, 591);
+            this.pbGridGeneration.Name = "pbGridGeneration";
+            this.pbGridGeneration.Size = new System.Drawing.Size(322, 39);
+            this.pbGridGeneration.TabIndex = 11;
+            // 
+            // GameOfLife
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1031, 692);
+            this.Controls.Add(this.pbGridGeneration);
+            this.Controls.Add(this.lblStats);
+            this.Controls.Add(this.btnSingleGeneration);
+            this.Controls.Add(this.btnLastGeneration);
+            this.Controls.Add(this.btnRandomGrid);
+            this.Controls.Add(this.btnClear);
+            this.Controls.Add(this.lblGridSize);
+            this.Controls.Add(this.numSize);
+            this.Controls.Add(this.lblSpeed);
+            this.Controls.Add(this.tbSpeed);
+            this.Controls.Add(this.btnStartAndStop);
+            this.Controls.Add(this.panContainer);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Margin = new System.Windows.Forms.Padding(4);
+            this.MaximizeBox = false;
+            this.Name = "GameOfLife";
+            this.Text = "Game of life";
+            ((System.ComponentModel.ISupportInitialize)(this.tbSpeed)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numSize)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panContainer;
+        private System.Windows.Forms.Button btnStartAndStop;
+        private System.Windows.Forms.TrackBar tbSpeed;
+        private System.Windows.Forms.Label lblSpeed;
+        private System.Windows.Forms.NumericUpDown numSize;
+        private System.Windows.Forms.Label lblGridSize;
+        private System.Windows.Forms.Button btnClear;
+        private System.Windows.Forms.Button btnRandomGrid;
+        private System.Windows.Forms.Button btnLastGeneration;
+        private System.Windows.Forms.Button btnSingleGeneration;
+        private System.Windows.Forms.Label lblStats;
+        private System.Windows.Forms.ProgressBar pbGridGeneration;
+    }
+}
+
---
+++ GameOfLife/GameOfLife/GameOfLife.cs
@@ -0,0 +1,426 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Windows.Forms;
+using Timer = System.Windows.Forms.Timer;
+
+namespace GameOfLife
+{
+    public partial class GameOfLife : Form
+    {
+        //Constants
+        private const int MIN_GRID_SIZE = 10;
+        private const int MAX_GRID_SIZE = 50;
+        private const int DEFAULT_GRID_SIZE = 25;
+
+        private const int MIN_SPEED = 1;
+        private const int MAX_SPEED = 250;
+        private const int DEFAULT_SPEED = 50;
+
+        private const string DEAD_CELL_TAG = "dead";
+        private const string ALIVE_CELL_TAG = "alive";
+        private readonly Color DEAD_CELL_COLOR = Color.White;
+        private readonly Color ALIVE_CELL_COLOR = Color.Black;
+
+        //Variables
+        private int gridSize = DEFAULT_GRID_SIZE;
+        private bool isGameRunning = false;
+
+        private int nbGenerations = 0;
+        private int nbCellsAlive = 0;
+        private int nbCellsDead = 0;
+
+        private Label[,] grid;
+        private Stack<List<Label>> lastsGenerations = new Stack<List<Label>>();
+        private Timer generationsTimer = new Timer();
+
+
+        public GameOfLife()
+        {
+            InitializeComponent();
+
+            //Confugure the taskbar for speed
+            tbSpeed.Minimum = MIN_SPEED;
+            tbSpeed.Maximum = MAX_SPEED;
+            tbSpeed.Value = DEFAULT_SPEED;
+            DisplaySpeed();
+
+            //Configure the input for the grid size
+            numSize.Minimum = MIN_GRID_SIZE;
+            numSize.Maximum = MAX_GRID_SIZE;
+            numSize.Value = DEFAULT_GRID_SIZE;
+            DisplayGridSize();
+        }
+
+        private void Init()
+        {
+            panContainer.Visible = false;
+            panContainer.SuspendLayout();
+
+            //Get the grid size
+            gridSize = (int)numSize.Value;
+
+            pbGridGeneration.Minimum = 1;
+            pbGridGeneration.Maximum = gridSize * gridSize;
+            pbGridGeneration.Value = 1;
+            pbGridGeneration.Step = 1;
+
+            //Clear the pannel
+            panContainer.Controls.Clear();
+
+            //Init the grid
+            grid = new Label[gridSize, gridSize];
+
+            //Generate the cells
+            for (int y = 0; y < grid.GetLength(0); y++)
+            {
+                for (int x = 0; x < grid.GetLength(1); x++)
+                {
+                    //Create the cell
+                    Label cell = new Label();
+
+                    //Set the size
+                    cell.Size = new Size(panContainer.Width / gridSize, panContainer.Height / gridSize);
+
+                    //Set the location
+                    cell.Location = new Point(x * cell.Width, y * cell.Height);
+
+                    //Set the color
+                    cell.BackColor = DEAD_CELL_COLOR;
+
+                    //Set the border
+                    cell.BorderStyle = BorderStyle.FixedSingle;
+
+                    //Set the index
+                    cell.TabStop = false;
+
+                    //Set the tag
+                    cell.Tag = DEAD_CELL_TAG;
+
+                    //Handle the click event
+                    cell.Click += new EventHandler(CellChangeState);
+
+                    //Add the cell to the container panel
+                    panContainer.Controls.Add(cell);
+
+                    //Add the cell in the grid array
+                    grid[y, x] = cell;
+
+                    //Dispaly the step on the progress bar
+                    pbGridGeneration.PerformStep();
+                }
+            }
+
+            //Display the stats
+            nbGenerations = 0;
+            nbCellsAlive = 0;
+            nbCellsDead = gridSize * gridSize;
+            DisplayStats();
+
+            panContainer.ResumeLayout();
+            panContainer.Visible = true;
+        }
+
+        private void CellChangeState(object sender, EventArgs e)
+        {
+            //Get the cell that cause the event
+            Label cell = (Label)sender;
+
+            //If the cell is dead
+            if((string)cell.Tag == DEAD_CELL_TAG)
+            {
+                //Set the cell to alive
+                cell.Tag = ALIVE_CELL_TAG;
+                cell.BackColor = ALIVE_CELL_COLOR;
+            }
+            //If the cell is alive
+            else if ((string)cell.Tag == ALIVE_CELL_TAG)
+            {
+                //Set the cell to dead
+                cell.Tag = DEAD_CELL_TAG;
+                cell.BackColor = DEAD_CELL_COLOR;
+            }
+        }
+
+        private void Generation()
+        {
+            //Set the number of alive and dead cell to 0 
+            nbCellsAlive = 0;
+            nbCellsDead = 0;
+
+            //Array with if the cells are alives
+            bool[,] gridIsAlive = new bool[gridSize, gridSize];
+
+            for (int y = 0; y < grid.GetLength(0); y++)
+            {
+                for (int x = 0; x < grid.GetLength(0); x++)
+                {
+                    //Get the number of neighbors
+                    int nbNeighbours = GetNumOfNeighbours(x, y);
+
+                    //Any live cell with two or three live neighbours survives
+                    if ((string)grid[y, x].Tag == ALIVE_CELL_TAG && (nbNeighbours == 2 || nbNeighbours == 3))
+                    {
+                        gridIsAlive[y, x] = true;
+                    }
+                    //Any dead cell with three live neighbours becomes a live cell.
+                    else if ((string)grid[y, x].Tag == DEAD_CELL_TAG && nbNeighbours == 3)
+                    {
+                        gridIsAlive[y, x] = true;
+                    }
+                    //All other live cells die in the next generation. Similarly, all other dead cells stay dead.
+                    else
+                    {
+                        gridIsAlive[y, x] = false;
+                    }
+                }
+            }
+
+
+            for (int y = 0; y < gridIsAlive.GetLength(0); y++)
+            {
+                for (int x = 0; x < gridIsAlive.GetLength(1); x++)
+                {
+                    //If the cell is alive
+                    if (gridIsAlive[y, x])
+                    {
+                        grid[y, x].Tag = ALIVE_CELL_TAG;
+                        grid[y, x].BackColor = ALIVE_CELL_COLOR;
+                        nbCellsAlive++;
+                    }
+                    //If the cell is dead
+                    else
+                    {
+                        grid[y, x].Tag = DEAD_CELL_TAG;
+                        grid[y, x].BackColor = DEAD_CELL_COLOR;
+                        nbCellsDead++;
+                    }
+                }
+            }
+
+
+            //Dispaly the stats
+            nbGenerations++;
+            DisplayStats();
+        }
+
+        private List<Label> GetCellsAlives()
+        {
+            //List with all alives cells
+            List<Label> cells = new List<Label>();
+
+            //Browse all the cells
+            foreach(Label lbl in panContainer.Controls)
+            {
+                //If the cell is alive
+                if((string)lbl.Tag == ALIVE_CELL_TAG)
+                {
+                    cells.Add(lbl);
+                }
+            }
+
+            return cells;
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="posX"></param>
+        /// <param name="posY"></param>
+        /// <returns></returns>
+        private int GetNumOfNeighbours(int posX, int posY)
+        {
+            int nbNeighbours = 0;
+            int[,] possibleCoords = new int[,] { {-1, -1 }, {0, -1 }, {1, -1 }, {-1, 0 }, {1, 0 }, {-1, 1 }, {0, 1 }, {1, 1 }};
+
+            for (int i = 0; i < possibleCoords.GetLength(0); i++)
+            {
+                if(posX + possibleCoords[i,0] >= 0 && posX + possibleCoords[i, 0] < gridSize && posY + possibleCoords[i, 1] >= 0 && posY + possibleCoords[i, 1] < gridSize)
+                {
+                    if ((string)grid[posY + possibleCoords[i, 1], posX + possibleCoords[i, 0]].Tag == "alive")
+                    {
+                        nbNeighbours++;
+                    }                    
+                }
+            }
+
+            return nbNeighbours;
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void btnStartAndStop_Click(object sender, EventArgs e)
+        {
+            //If the game isn't running
+            if (!isGameRunning)
+            {
+                //Start the game
+                Start();
+            }
+            //If the game is running
+            else
+            {
+                //Stop the game
+                Stop();
+            }
+
+        }
+
+        private void Start()
+        {
+            isGameRunning = true;
+            generationsTimer.Interval = tbSpeed.Value;
+            generationsTimer.Tick += new EventHandler(TimerTick);
+            generationsTimer.Start();
+            btnStartAndStop.Text = "Stop";
+        }
+
+        private void Stop()
+        {
+            isGameRunning = false;
+            generationsTimer.Stop();
+            generationsTimer = new Timer();
+            btnStartAndStop.Text = "Start";
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void TimerTick(object sender, EventArgs e)
+        {
+            //Put all cells alives in the stack
+            lastsGenerations.Push(GetCellsAlives());
+
+            //Start 1 generation
+            Generation();
+        }
+
+        private void tbSpeed_Scroll(object sender, EventArgs e)
+        {
+            //Change the tick interval of the timer with the new speed
+            generationsTimer.Interval = tbSpeed.Value;
+
+            //Display the new speed
+            DisplaySpeed();
+        }
+
+        private void NumSize_ValueChanged(object sender, EventArgs e)
+        {
+            //Stop the game
+            Stop();
+
+            //Reset the lasts generations
+            lastsGenerations = new Stack<List<Label>>();
+
+            //Iinitialize the game
+            Init();
+
+            //Display the size of the grid
+            DisplayGridSize();
+        }
+
+        private void BtnClear_Click(object sender, EventArgs e)
+        {
+            //Stop the game
+            Stop();
+
+            //Reset the lasts generations
+            lastsGenerations = new Stack<List<Label>>();
+
+            //Set all cells to dead
+            foreach (Label lbl in panContainer.Controls)
+            {
+                lbl.Tag = DEAD_CELL_TAG;
+                lbl.BackColor = DEAD_CELL_COLOR;
+            }
+
+            //Dispaly the stats
+            nbGenerations = 0;
+            nbCellsAlive = 0;
+            nbCellsDead = gridSize * gridSize;
+            DisplayStats();
+        }
+
+        private void BtnRandomGrid_Click(object sender, EventArgs e)
+        {
+            Random rnd = new Random();
+
+            //Browse all the cells
+            foreach (Label lbl in panContainer.Controls)
+            {
+                //40% of chance of being alive
+                if(rnd.Next(0, 100) < 40)
+                {
+                    //Set the cell to alive
+                    lbl.Tag = ALIVE_CELL_TAG;
+                    lbl.BackColor = ALIVE_CELL_COLOR;
+                }
+                else
+                {
+                    //Set the cell to dead
+                    lbl.Tag = DEAD_CELL_TAG;
+                    lbl.BackColor = DEAD_CELL_COLOR;
+                }
+                
+            }
+        }
+
+        private void BtnLastGeneration_Click(object sender, EventArgs e)
+        {
+            //If the stack with the lasts generations isn't empty
+            if(lastsGenerations.Count > 0)
+            {
+                //Get all alives cells from the last generation
+                List<Label> aliveCells = lastsGenerations.Pop();
+
+                //Browse all the alive cells
+                foreach (Label lbl in panContainer.Controls)
+                {
+                    //If the cell is in the alives from the last generation
+                    if (aliveCells.Contains(lbl))
+                    {
+                        //Set the cell to alive
+                        lbl.Tag = ALIVE_CELL_TAG;
+                        lbl.BackColor = ALIVE_CELL_COLOR;
+                    }
+                    else
+                    {
+                        //Set the cell to dead
+                        lbl.Tag = DEAD_CELL_TAG;
+                        lbl.BackColor = DEAD_CELL_COLOR;
+                    }
+                }
+            }
+            
+        }
+
+        private void BtnSingleGeneration_Click(object sender, EventArgs e)
+        {
+            //Start 1 generation
+            TimerTick(null, null);
+        }
+
+        private void DisplayStats()
+        {
+            lblStats.Text = $"" +
+                $"Nombre de générations : {nbGenerations}\n" +
+                $"Nombre de cellules vivantes : {nbCellsAlive}\n" +
+                $"Nombre de cellules mortes : {nbCellsDead}";
+        }
+
+        private void DisplayGridSize()
+        {
+            lblGridSize.Text = $"Taille de la grille : {numSize.Value}";
+        }
+
+        private void DisplaySpeed()
+        {
+            lblSpeed.Text = $"Speed : {tbSpeed.Value}ms";
+        }
+    }
+}
